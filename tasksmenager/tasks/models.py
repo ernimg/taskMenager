0078edc99a07d5484e62098bd88ec65b9cf1e392
ernimg <mglernest@gmail.com>
2022-11-09 22:44:22 +0100

    create models and migrate

---
+++ tasksmenager/tasks/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+PRIORITY_STATUS = (
+    ('low', 'LOW'),
+    ('medium', 'MEDIUM'),
+    ('height', 'HEIGHT')
+)
+
+# Create your models here.
+class Tasks(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    files = models.FileField(upload_to='uploads/')
+    URL = models.URLField()
+    priority = models.CharField(choices = PRIORITY_STATUS ,max_length = 30, default = 'low')
+
+class Vote(models.Model):
+    # Asocjacja
+    tasks = models.ForeignKey(Tasks, on_delete = models.CASCADE)
+    usersVote = models.TextField
